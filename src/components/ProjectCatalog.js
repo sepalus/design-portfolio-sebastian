a73a0401f3bb7c8b236d92ad0878fa1c78c72cbd
Sebastian Hognabba <seba@Sebastians-MBP.lan>
2019-10-24 13:13:08 +0300

    Product catalog styling

--- src/components/ProjectCatalog.js
+++ src/components/ProjectCatalog.js
@@ -1,14 +1,30 @@
 import React from "react";
 
+const productDesignProjects = [
+  { title: "Kaarna", subtitle: "Vehicle Design" },
+  { title: "Puro", subtitle: "Acoustic Design" },
+  { title: "Kuu", subtitle: "Light Design" }
+];
+
 function ProjectCatalog() {
   return (
-    <div>
-      <ul>
-        <li>Kaarna</li>
-        <li>Puro</li>
-        <li>Kuu</li>
+    <div className="project-catalog">
+      <h2>Product Design</h2>
+      <ul className="project-catalog-projects">
+        {productDesignProjects.map(project => (
+          <CatalogueItem title={project.title} subtitle={project.subtitle} />
+        ))}
       </ul>
     </div>
+  );
+}
+
+function CatalogueItem({ title, subtitle }) {
+  return (
+    <li>
+      <h3>{title}</h3>
+      <p>{subtitle}</p>
+    </li>
   );
 }
 
--- src/components/ProjectSection.js
+++ src/components/ProjectSection.js
@@ -1,10 +1,18 @@
 import React from "react";
+import classNames from "classnames";
+
 import ProjectCatalog from "./ProjectCatalog";
 import ProjectCard from "./ProjectCard";
+import "./ProjectSection.scss";
 
 function ProjectSection({ projectRef, currentDesignArea, selectViewport }) {
   return (
-    <section ref={projectRef}>
+    <section
+      ref={projectRef}
+      className={classNames("project-selection-container", {
+        "project-selection-digital": currentDesignArea !== 0
+      })}
+    >
       <ProjectCatalog />
       <ProjectCard />
       <button onClick={() => selectViewport(0)}>Home</button>
