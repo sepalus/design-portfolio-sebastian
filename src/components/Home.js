48b72b80cd70229fb5ab5fbed0fec3505ab7656b
Sebastian Hognabba <seba@Sebastians-MBP.lan>
2019-12-19 10:42:20 +0200

    Add event listener to set viewport based on scroll position

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,10 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import DesignerSection from "./DesignerSection";
 import ProjectSection from "./ProjectSection";
 import InformationSection from "./InformationSection";
 
 export default function Home() {
+  const [previousYOffset, setPreviousYOffset] = useState(0);
   const [isInitialViewPort, setIsInitialViewPort] = useState(true);
   const [currentViewport, setCurrentViewport] = useState(0); // 0 === 'designer', 1 === 'project'
   const [currentDesignArea, setCurrentDesignArea] = useState(0); // 0 === 'industrial', 1 === 'digital'
@@ -12,6 +13,33 @@ export default function Home() {
   const designerRef = useRef(null);
   const projectRef = useRef(null);
   const informationRef = useRef(null);
+  const rootElement = document.getElementById("root");
+
+  useEffect(() => {
+    const detectViewportOnScroll = () => {
+      const currentYOffset = rootElement.scrollTop;
+      const projectRefOffset = projectRef.current.offsetTop;
+      const informationRefOffset = informationRef.current.offsetTop;
+
+      setPreviousYOffset(currentYOffset);
+
+      const nextViewport =
+        currentYOffset >= informationRefOffset
+          ? 2
+          : currentYOffset >= projectRefOffset
+          ? 1
+          : 0;
+
+      if (currentViewport !== nextViewport) {
+        setShouldAnimate(true);
+        setCurrentViewport(nextViewport);
+        setTimeout(() => setIsInitialViewPort(false), 500);
+      }
+    };
+    rootElement.addEventListener("scroll", detectViewportOnScroll);
+    return () =>
+      rootElement.removeEventListener("scroll", detectViewportOnScroll);
+  }, [currentViewport, previousYOffset, rootElement]);
 
   const selectViewport = viewport => {
     const ref =
