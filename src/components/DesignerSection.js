f661ba489c66590c030796190f6470d036fd7bcb
Sebastian Högnabba <sebastian.hognabba@gmail.com>
2020-09-30 19:59:31 +0300

    Designer section styling + typist fix

--- src/components/DesignerSection.js
+++ src/components/DesignerSection.js
@@ -17,24 +17,27 @@ function DesignerSection({
     <section ref={designerRef} className="designer-section-container">
       <div className="designer-section-items">
         {showTypist ? (
-          <Typist
-            className="designer-section-typist"
-            startDelay={700}
-            avgTypingDelay={20}
-            stdTypingDelay={0}
-            cursor={{
-              hideWhenDone: true,
-              hideWhenDoneDelay: 0,
-            }}
-          >
-            <h3>
+          <h3>
+            <Typist
+              className="designer-section-typist"
+              startDelay={700}
+              avgTypingDelay={20}
+              stdTypingDelay={0}
+              cursor={{
+                hideWhenDone: true,
+                hideWhenDoneDelay: 0,
+              }}
+              onTypingDone={() =>
+                setTimeout(() => setShowWorkTypist(true), 500)
+              }
+            >
               Hello, I am&nbsp;
               <span className="link" onClick={() => selectViewport(2)}>
                 Sebastian
               </span>
               &nbsp;a designer for the physical and digital world.
-            </h3>
-          </Typist>
+            </Typist>
+          </h3>
         ) : (
           <div className="designer-section-typist">
             <h3>
@@ -48,35 +51,35 @@ function DesignerSection({
         )}
         {showTypist ? (
           showWorkTypist && (
-            <Typist
-              className="designer-section-typist"
-              startDelay={200}
-              avgTypingDelay={20}
-              stdTypingDelay={0}
-              cursor={{
-                hideWhenDone: true,
-                hideWhenDoneDelay: 0,
-              }}
-            >
-              <h3>
+            <h3>
+              <Typist
+                className="designer-section-typist"
+                startDelay={200}
+                avgTypingDelay={20}
+                stdTypingDelay={0}
+                cursor={{
+                  hideWhenDone: true,
+                  hideWhenDoneDelay: 0,
+                }}
+              >
                 If you’re interested in what I’ve done, please have a look at
                 my&nbsp;
                 <span className="link" onClick={() => selectViewport(1)}>
                   work
                 </span>
-              </h3>
-            </Typist>
+              </Typist>
+            </h3>
           )
         ) : (
-          <div className="designer-section-typist">
-            <h3>
+          <h3>
+            <div className="designer-section-typist">
               If you’re interested in what I’ve done, please have a look at
               my&nbsp;
               <span className="link" onClick={() => selectViewport(1)}>
                 work
               </span>
-            </h3>
-          </div>
+            </div>
+          </h3>
         )}
       </div>
     </section>
