706d5cdcc8c5758b95f0a968a3b802086600a24c
Sebastian Högnabba <sebastian.hognabba@gmail.com>
2020-09-30 11:07:06 +0300

    Restyle designer page

--- src/components/DesignerSection.js
+++ src/components/DesignerSection.js
@@ -4,15 +4,6 @@ import Typist from "react-typist";
 import KeyboardBackspace from "@material-ui/icons/KeyboardBackspace";
 import "./DesignerSection.scss";
 
-const ItemLink = ({ className, title, onClick }) => (
-  <h1
-    className={classNames("link", "designer-section-menu", className)}
-    onClick={onClick}
-  >
-    {title}
-  </h1>
-);
-
 function DesignerSection({
   designerRef,
   showTypist,
@@ -24,82 +15,70 @@ function DesignerSection({
 
   return (
     <section ref={designerRef} className="designer-section-container">
-      <img
-        className="designer-section-image"
-        src="../assets/face_industrial_transparent.png"
-        alt="Sebastian"
-      />
-      {showTypist ? (
-        <Typist
-          className="designer-section-menu designer-section-typist designer-section-typist-about"
-          startDelay={700}
-          avgTypingDelay={50}
-          stdTypingDelay={0}
-          cursor={{
-            hideWhenDone: true,
-            hideWhenDoneDelay: 0,
-          }}
-          onTypingDone={setTimeout(() => setShowWorkTypist(true), 3500)}
-        >
-          <h1>
-            Hello, I am
-            <br />
-            <span className="link" onClick={() => selectViewport(2)}>
-              Sebastian
-            </span>
-            <KeyboardBackspace
-              className="designer-section-text-arrow designer-section-text-arrow-forward"
-              fontSize="large"
-              style={{ fontSize: 48 }}
-            />
-          </h1>
-          {""}
-        </Typist>
-      ) : (
-        <ItemLink
-          className="designer-section-typist-about"
-          title="Sebastian"
-          onClick={() => selectViewport(2)}
-        />
-      )}
-      {showTypist ? (
-        showWorkTypist && (
+      <div className="designer-section-items">
+        {showTypist ? (
           <Typist
-            className="designer-section-menu designer-section-typist designer-section-typist-work"
-            startDelay={200}
-            avgTypingDelay={50}
+            className="designer-section-typist"
+            startDelay={700}
+            avgTypingDelay={25}
             stdTypingDelay={0}
             cursor={{
               hideWhenDone: true,
               hideWhenDoneDelay: 0,
             }}
           >
-            <h1>
-              Please
-              <br />
-              check
-              <br />
-              out my
-              <br />
+            <h3>
+              Hello, I am&nbsp;
+              <span className="link" onClick={() => selectViewport(2)}>
+                Sebastian
+              </span>
+              &nbsp;a designer for the physical and digital world.
+            </h3>
+          </Typist>
+        ) : (
+          <div className="designer-section-typist">
+            <h3>
+              Hello, I am&nbsp;
+              <span className="link" onClick={() => selectViewport(2)}>
+                Sebastian
+              </span>
+              &nbsp;a designer for the physical and digital world.
+            </h3>
+          </div>
+        )}
+        {showTypist ? (
+          showWorkTypist && (
+            <Typist
+              className="designer-section-typist"
+              startDelay={200}
+              avgTypingDelay={25}
+              stdTypingDelay={0}
+              cursor={{
+                hideWhenDone: true,
+                hideWhenDoneDelay: 0,
+              }}
+            >
+              <h3>
+                If you’re interested in what I’ve done, please have a look at
+                my&nbsp;
+                <span className="link" onClick={() => selectViewport(1)}>
+                  work
+                </span>
+              </h3>
+            </Typist>
+          )
+        ) : (
+          <div className="designer-section-typist">
+            <h3>
+              If you’re interested in what I’ve done, please have a look at
+              my&nbsp;
               <span className="link" onClick={() => selectViewport(1)}>
                 work
               </span>
-              <KeyboardBackspace
-                className="designer-section-text-arrow designer-section-text-arrow-down"
-                fontSize="large"
-                style={{ fontSize: 48 }}
-              />
-            </h1>{" "}
-            {""}
-          </Typist>
-        )
-      ) : (
-        <ItemLink
-          className="designer-section-typist-work"
-          title="Work"
-          onClick={() => selectViewport(1)}
-        />
-      )}
+            </h3>
+          </div>
+        )}
+      </div>
     </section>
   );
 }
