9fc99ec201e5e39c000d8f5a570f7ff7cd7d2a3f
Sebastian Högnabba <sebastian.hognabba@gmail.com>
2020-10-24 12:26:53 +0300

    Designer part refactor

--- src/components/DesignerSection.js
+++ src/components/DesignerSection.js
@@ -1,92 +1,97 @@
 import React, { useState } from "react";
 import classNames from "classnames";
 import Typist from "react-typist";
-import KeyboardBackspace from "@material-ui/icons/KeyboardBackspace";
 import "./DesignerSection.scss";
 
 function DesignerSection({
   designerRef,
   showTypist,
   selectViewport,
-  currentDesignArea,
-  selectDesignArea,
   setSkipProjectSection,
 }) {
-  const [showWorkTypist, setShowWorkTypist] = useState(false);
+  const [aboutTypistDone, setAboutTypistDone] = useState(false);
+  const [workTypistDone, setWorkTypistDone] = useState(false);
 
   return (
     <section ref={designerRef} className="designer-section-container">
-      <div className="designer-section-items">
-        {showTypist ? (
-          <h3>
-            <Typist
-              className="designer-section-typist"
-              startDelay={700}
-              avgTypingDelay={20}
-              stdTypingDelay={0}
-              cursor={{
-                hideWhenDone: true,
-                hideWhenDoneDelay: 0,
-              }}
-              onTypingDone={() =>
-                setTimeout(() => setShowWorkTypist(true), 500)
-              }
+      <div className="designer-section-content">
+        <div className="designer-section-typist-wrapper">
+          {showTypist ? (
+            <h3
+              className={classNames({
+                "typist-button-disabled": !aboutTypistDone,
+              })}
             >
-              Hello, I am&nbsp;
-              <span
-                className="link-button link-button-narrow"
-                onClick={() => {
-                  setSkipProjectSection(true);
-                  selectViewport(2);
-                }}
-              >
-                Sebastian
-              </span>
-              &nbsp;a designer for the physical and digital world
-            </Typist>
-          </h3>
-        ) : (
-          <div className="designer-section-typist">
-            <h3>
-              Hello, I am&nbsp;
-              <span
-                className="link-button link-button-narrow"
-                onClick={() => selectViewport(2)}
-              >
-                Sebastian
-              </span>
-              &nbsp;a designer for the physical and digital world
-            </h3>
-          </div>
-        )}
-        {showTypist ? (
-          showWorkTypist && (
-            <h3>
               <Typist
-                className="designer-section-typist"
-                startDelay={200}
+                startDelay={700}
                 avgTypingDelay={20}
                 stdTypingDelay={0}
                 cursor={{
                   hideWhenDone: true,
                   hideWhenDoneDelay: 0,
                 }}
+                onTypingDone={() =>
+                  setTimeout(() => setAboutTypistDone(true), 500)
+                }
               >
-                If you’re interested in what I’ve done, please have a look at
-                my&nbsp;
+                Hello, I'm&nbsp;
                 <span
                   className="link-button link-button-narrow"
-                  onClick={() => selectViewport(1)}
+                  onClick={() => {
+                    setSkipProjectSection(true);
+                    selectViewport(2);
+                  }}
                 >
-                  work
+                  Sebastian
                 </span>
+                &nbsp;a designer in both the physical and digital world
               </Typist>
             </h3>
-          )
-        ) : (
-          <h3>
-            <div className="designer-section-typist">
-              If you’re interested in what I’ve done, please have a look at
+          ) : (
+            <h3>
+              Hello, I'm&nbsp;
+              <span
+                className="link-button link-button-narrow"
+                onClick={() => selectViewport(2)}
+              >
+                Sebastian
+              </span>
+              &nbsp;a designer in both the physical and digital world
+            </h3>
+          )}
+          {showTypist ? (
+            aboutTypistDone && (
+              <h3
+                className={classNames({
+                  "typist-button-disabled": !workTypistDone,
+                })}
+              >
+                <Typist
+                  startDelay={200}
+                  avgTypingDelay={20}
+                  stdTypingDelay={0}
+                  cursor={{
+                    hideWhenDone: true,
+                    hideWhenDoneDelay: 0,
+                  }}
+                  onTypingDone={() =>
+                    setTimeout(() => setWorkTypistDone(true), 500)
+                  }
+                >
+                  If you’re interested in what I’ve done, please take a look at
+                  my&nbsp;
+                  <span
+                    className="link-button link-button-narrow"
+                    onClick={() => selectViewport(1)}
+                  >
+                    work
+                  </span>
+                </Typist>
+              </h3>
+            )
+          ) : (
+            <h3>
+              If you’re interested in what I’ve done, please take a look at
               my&nbsp;
               <span
                 className="link-button link-button-narrow"
@@ -94,9 +99,9 @@ function DesignerSection({
               >
                 work
               </span>
-            </div>
-          </h3>
-        )}
+            </h3>
+          )}
+        </div>
       </div>
     </section>
   );
